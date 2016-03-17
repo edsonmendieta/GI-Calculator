6a552260207981c7fb11ad5066d0c361f04da474
emendietaj <emendietaj@gmail.com>
2016-03-17 04:12:42 -0400

    scripts, styles, html, files

--- main.js
+++ main.js
@@ -6,5 +6,33 @@ $(document).ready(function() {
   
   
   
+  $('input').iCheck({
+    checkboxClass: 'icheckbox_flat-green',
+    radioClass: 'iradio_flat-green',
+    increaseArea: '20%',
+  });
+  
+  
+  
+  // Dynamically adds checked food item to breakfast list 
+  $('input').on('ifChecked', function(event) {       
+    var liText = $(this).parent().parent().text();
+    $('.food-list').append('<li class=>' + liText + '</li>');
+})
+  
+  // Dynamically removes un-checked food item from breakfast list
+  $('input').on('ifUnchecked', function(event) {
+    var liText = $(this).parent().parent().text();
+    $('li:contains(' + liText + ')').remove();
+})
+  
+
+
+  
+
+  
+  
+  
+  
   
 });
