7e418191205385964161efc2834b9d427311e282
emendietaj <emendietaj@gmail.com>
2016-03-20 20:29:41 -0400

    added fatsecret API

--- main.js
+++ main.js
@@ -3,14 +3,23 @@ $(document).ready(function() {
   
   $(".page-header").fitText()
   
+  // Initializes Fat Scecret API
+fatsecret.setContainer("my_container");
+fatsecret.setCanvas("home");
   
   
   
+  // Initialisizes icheck.js and sets styles of checkboxes
   $('input').iCheck({
     checkboxClass: 'icheckbox_flat-green',
     radioClass: 'iradio_flat-green',
     increaseArea: '20%',
   });
+  
+  // initilizes Bootstrap Tooltips
+  $(function () {
+    $('[data-toggle="tooltip"]').tooltip()
+  })
   
   
   
@@ -18,6 +27,7 @@ $(document).ready(function() {
   $('input').on('ifChecked', function(event) {       
     var liText = $(this).parent().parent().text();
     $('.food-list').append('<li class=>' + liText + '</li>');
+    
 })
   
   // Dynamically removes un-checked food item from breakfast list
@@ -26,6 +36,8 @@ $(document).ready(function() {
     $('li:contains(' + liText + ')').remove();
 })
   
+
+  $('.fatsecret_home_search_input').val(poop);
 
 
   
